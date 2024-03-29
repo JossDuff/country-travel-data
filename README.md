9889846799ffa4ca773b657fe5e9a21e345db5f3
jossduff <jduff360@gmail.com>
2024-03-29 16:10:09 +0100

    better hdi source and general structure

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Some data and analysis to help me figure out what countries I want to visit.
 - `crime-rate-by-country-2024.csv` [https://worldpopulationreview.com/country-rankings/crime-rate-by-country](https://worldpopulationreview.com/country-rankings/crime-rate-by-country)
 - `murder-rate-by-country-2024.csv` [https://worldpopulationreview.com/country-rankings/murder-rate-by-country](https://worldpopulationreview.com/country-rankings/murder-rate-by-country)
 - `most-visited-countries-2024.csv` [https://worldpopulationreview.com/country-rankings/most-visited-countries](https://worldpopulationreview.com/country-rankings/most-visited-countries)
-- `human_development_index.xlsx` [https://hdr.undp.org/data-center/documentation-and-downloads](https://hdr.undp.org/data-center/documentation-and-downloads)
+- `human_development-index-by-country-2024` (this is actually hdi from 2021) [https://worldpopulationreview.com/country-rankings/hdi-by-country](https://worldpopulationreview.com/country-rankings/hdi-by-country)
--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import pandas as pd
 
-cost_of_living_file = 'cost-of-living-by-country-2024.csv'
-crime_rate_file = 'crime-rate-by-country-2024.csv'
-murder_rate_file = 'murder-rate-by-country-2024.csv'
+cost_of_living_file = 'data/cost-of-living-by-country-2024.csv'
+crime_rate_file = 'data/crime-rate-by-country-2024.csv'
+murder_rate_file = 'data/murder-rate-by-country-2024.csv'
+most_visited_file = 'data/most-visited-countries-2024.csv'
+human_development_index_file = 'data/human-development-index-by-country-2024.csv'
 
+cost_of_living_df = pd.read_csv(cost_of_living_file)
+crime_rate_df = pd.read_csv(crime_rate_file)
+murder_rate_df = pd.read_csv(murder_rate_file)
+most_visited_df = pd.read_csv(most_visited_file)
+hdi_df = pd.read_csv(human_development_index_file)
 
-col_df = pd.read_csv(cost_of_living_file)
+dataframes = {
+    "cost_of_living": cost_of_living_df,
+    "crime_rate": crime_rate_df,
+    "murder_rate": murder_rate_df,
+    "most_visited": most_visited_df,
+    "hdi": hdi_df
+}
 
-
-
-# Display the first few rows of the DataFrame
-print(col_df.head())
+for name, df in dataframes.items():
+    print(f"{name} data:")
+    print("columns:", df.columns.tolist())
+    print("num countries:", df.shape[0], "\n")
